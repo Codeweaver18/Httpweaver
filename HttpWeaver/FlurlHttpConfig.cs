6361f5b472d2b623a45c6635d394f76dbac08e7f
MATSUMU MONDAY <jereco11@gmail.com>
2018-11-10 22:30:01 +0100

    Httpweaver commit

---
+++ HttpWeaver/FlurlHttpConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpWeaver
+{
+   public class FlurlHttpConfig
+    {
+        public const int REST_REQUEST_TIMEOUT = 60;
+
+    }
+}
